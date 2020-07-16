a0747f2cfedc62e83106bc8b06b127b30c354600
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-16 14:26:02 +0530

    feat: Effect Stopped

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -1,35 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { AuthContextProvider } from './AuthContext'
-import TokenObject from './TokenObject'
 
 type AuthProps = {
-  cookieName: string
   children: any
 }
 
-const AuthProvider: React.FunctionComponent<AuthProps> = ({
-  cookieName,
-  children
-}) => {
-  const JwtTokenObject = new TokenObject(cookieName)
-  const [authObject, setAuthObject] = useState<TokenInterface>(
-    JwtTokenObject.getToken()
-  )
+const AuthProvider: React.FunctionComponent<AuthProps> = ({ children }) => {
+  const [authObject, setAuthObject] = useState<TokenInterface>({
+    authToken: null,
+    expireAt: null
+  })
 
-  useEffect(() => {
-    console.log('Using Auth Effect')
-    if (
-      authObject.authToken === undefined ||
-      authObject.authToken === null ||
-      authObject.expireAt === null
-    ) {
-      console.log('Using Auth Effect removing Token')
-      JwtTokenObject.removeToken()
-    } else {
-      console.log('Using Auth Effect Adding Token')
-      JwtTokenObject.setToken(authObject.authToken, authObject.expireAt)
-    }
-  }, [authObject])
+  // useEffect(() => {
+  //   console.log('Using Auth Effect')
+  //   if (authObject.authToken === null || authObject.expireAt === null) {
+  //     console.log('Using Auth Effect removing Token')
+  //     JwtTokenObject.removeToken()
+  //   } else {
+  //     console.log('Using Auth Effect Adding Token')
+  //     JwtTokenObject.setToken(authObject.authToken, authObject.expireAt)
+  //   }
+  // }, [authObject])
 
   return (
     <AuthContextProvider
