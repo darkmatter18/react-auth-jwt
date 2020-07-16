0763d1ca97ae0d95088c3a0a26b3720eb279f6e1
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-16 20:04:24 +0530

    feat: Effect added

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AuthContextProvider } from './AuthContext'
+import TokenObject from './TokenObject'
 
 type AuthProps = {
+  cookieName: string
   children: React.ReactChildren
 }
 
-const AuthProvider: React.FunctionComponent<AuthProps> = ({ children }) => {
-  const [authState, setAuthState] = useState<TokenInterface>({
-    authToken: null,
-    expireAt: null
-  })
+const AuthProvider: React.FunctionComponent<AuthProps> = ({
+  children,
+  cookieName
+}) => {
+  const JwtTokenObject = new TokenObject(cookieName)
+  const [authState, setAuthState] = useState<TokenInterface>(
+    JwtTokenObject.getToken()
+  )
+
+  useEffect(() => {
+    console.log('RAJ :: Using Auth Effect')
+    if (
+      authState.authToken === undefined ||
+      authState.authToken === null ||
+      authState.expireAt === null
+    ) {
+      console.log('RAJ :: Using Auth Effect removing Token')
+      JwtTokenObject.removeToken()
+    } else {
+      console.log('RAJ :: Using Auth Effect Adding Token')
+      JwtTokenObject.setToken(authState.authToken, authState.expireAt)
+    }
+  }, [authState])
 
   return (
     <AuthContextProvider value={{ authState, setAuthState }}>
