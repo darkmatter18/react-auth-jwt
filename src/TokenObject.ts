7d72db7df1c43f44b2844734acff2722d0e87244
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-24 12:22:42 +0530

    JS cookies added

--- src/TokenObject.ts
+++ src/TokenObject.ts
@@ -1,43 +1,64 @@
+import Cookies from 'js-cookie'
+
 class TokenObject {
-  private readonly tokenName: string
-  constructor(tokenName: string) {
-    this.tokenName = tokenName
+  private readonly authCookieName: string
+  private readonly stateCookieName: string
+  private readonly authTimeCookieName: string
+  private readonly cookieDomain: string
+  private readonly cookieSecure: boolean
+
+  constructor(
+    authCookieName: string,
+    authTimeCookieName: string,
+    stateCookieName: string,
+    cookieDomain: string,
+    cookieSecure: boolean
+  ) {
+    this.authCookieName = authCookieName
+    this.authTimeCookieName = authTimeCookieName
+    this.stateCookieName = stateCookieName
+    this.cookieDomain = cookieDomain
+    this.cookieSecure = cookieSecure
   }
 
-  isTokenExist() {
-    return localStorage[this.tokenName] !== undefined
-  }
-
-  isTokenExpire() {
-    if (this.isTokenExist()) {
-      const _e = JSON.parse(localStorage[this.tokenName]).expiresAt
-      return _e < new Date()
-    } else {
-      return true
-    }
-  }
-
-  setToken(value: string, expiresAt: Date, authState: object) {
-    // {value: xxx, expiresAt: ttt}
-    const json = { value: value, expiresAt: expiresAt, authState: authState }
-    localStorage[this.tokenName] = JSON.stringify(json)
+  setToken(authToken: string, expiresAt: Date, authState: object) {
+    Cookies.set(this.authCookieName, authToken, {
+      expires: expiresAt,
+      domain: this.cookieDomain,
+      secure: this.cookieSecure
+    })
+    Cookies.set(this.authTimeCookieName, expiresAt, {
+      expires: expiresAt,
+      domain: this.cookieDomain,
+      secure: this.cookieSecure
+    })
+    Cookies.set(this.stateCookieName, authState, {
+      expires: expiresAt,
+      domain: this.cookieDomain,
+      secure: this.cookieSecure
+    })
   }
 
   removeToken() {
-    localStorage.removeItem(this.tokenName)
+    Cookies.remove(this.authCookieName)
+    Cookies.remove(this.authTimeCookieName)
+    Cookies.remove(this.stateCookieName)
   }
 
   initialToken(): TokenInterface {
-    if (this.isTokenExist()) {
-      if (!this.isTokenExpire()) {
-        const _t = JSON.parse(localStorage[this.tokenName])
-        const token = _t.value
-        const expiresAt = _t.expiresAt
-        const authState = _t.authState
-        return { authToken: token, expireAt: expiresAt, authState: authState }
-      } else {
-        return { authToken: null, expireAt: null, authState: null }
-      }
+    const authCookie = Cookies.get(this.authCookieName)
+    const authTimeCookie = Cookies.get(this.authTimeCookieName)
+    const stateCookie = Cookies.get(this.stateCookieName)
+
+    if (
+      authCookie !== undefined &&
+      authTimeCookie !== undefined &&
+      stateCookie !== undefined
+    ) {
+      const token = authCookie
+      const expiresAt = new Date(authTimeCookie)
+      const authState = JSON.parse(stateCookie)
+      return { authToken: token, expireAt: expiresAt, authState: authState }
     } else {
       return { authToken: null, expireAt: null, authState: null }
     }
