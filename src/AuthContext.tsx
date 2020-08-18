de7569c6e7b27d36a9e4b5ee13ab5c5d3d837839
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-18 19:58:29 +0530

    feat: ContextProps type moved to AuthContext.tsx

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+declare interface ContextProps {
+  authState: TokenInterface
+  setAuthState: React.Dispatch<React.SetStateAction<TokenInterface>>
+}
+
 const Auth = React.createContext<ContextProps | null>(null)
 
 export const AuthContextProvider = Auth.Provider
