136647e6f9a4fb52fbc86adcbee0bb21cd5cfcab
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-18 19:59:51 +0530

    feat: TokenInterface Added in useAuth.ts

--- src/useAuth.ts
+++ src/useAuth.ts
@@ -1,6 +1,12 @@
 import { useContext } from 'react'
 import { AuthContext } from './AuthContext'
 
+declare interface TokenInterface {
+  authToken: authTokenType
+  expireAt: Date | null
+  authState: object | null
+}
+
 /**
  * Auth State Hook
  *
